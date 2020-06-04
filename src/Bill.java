534e06be3937da258700d81b22f236f9dbcd8cc4
Mark Sinakaev <high-school@bk.ru>
2020-06-04 21:55:37 +0300

    added small refactoring

--- src/Bill.java
+++ src/Bill.java
@@ -23,58 +23,57 @@ public class Bill {
         int totalBonus = 0;
         Iterator<Item> items = _items.iterator();
 
-        String result = "Счет для " + _customer.getName() + "\n";
-        result += "\t" + "Название" + "\t" + "Цена" +
-                "\t" + "Кол-во" + "Стоимость" + "\t" + "Скидка" +
-                "\t" + "Сумма" + "\t" + "Бонус" + "\n";
-
+        String result = tableHeaderOutput();
 
         while (items.hasNext()) {
             double thisAmount = 0;
             double discount = 0;
+            double sumTotal = 0;
             int bonus = 0;
             Item each = (Item) items.next();
-
-
+            sumTotal = each.getQuantity() * each.getPrice();
             switch (each.getGoods().getPriceCode()) {
-
                 case Goods.REGULAR:
                     if (each.getQuantity() > 2)
-                        discount = (each.getQuantity() * each.getPrice()) * 0.03; // 3%
+                        discount = sumTotal * 0.03;
                     bonus = (int) (each.getQuantity() * each.getPrice() * 0.05);
                     break;
-
                 case Goods.SPECIAL_OFFER:
                     if (each.getQuantity() > 10)
-                        discount = (each.getQuantity() * each.getPrice()) * 0.005; // 0.5%
+                        discount = sumTotal * 0.005; // 0.5%
                     break;
                 case Goods.SALE:
                     if (each.getQuantity() > 3)
-                        discount = (each.getQuantity() * each.getPrice()) * 0.01; // 0.1%
-                    bonus = (int) (each.getQuantity() * each.getPrice() * 0.01);
+                        discount = sumTotal * 0.01; // 0.1%
+                    bonus = (int) (sumTotal * 0.01);
                     break;
             }
-            // сумма
-            thisAmount = each.getQuantity() * each.getPrice();
-            // используем бонусы
-            if ((each.getGoods().getPriceCode() == Goods.SPECIAL_OFFER) && each.getQuantity() > 1)
-                discount = _customer.useBonus((int) (each.getQuantity() * each.getPrice()));
-            // учитываем скидку
-            thisAmount = each.getQuantity() * each.getPrice() - discount;
-            //показать результаты
+            if (isDiscounting(each))
+                discount = _customer.useBonus((int) (sumTotal));
+            thisAmount = sumTotal - discount;
             result += "\t" + each.getGoods().getTitle() + "\t" +
                     "\t" + each.getPrice() + "\t" + each.getQuantity() +
-                    "\t" + Double.toString(each.getQuantity() * each.getPrice()) +
-                    "\t" + discount + "\t" + thisAmount +
+                    "\t" + "\t" + each.getQuantity() * each.getPrice() +
+                    "\t"  + discount + "\t"  + thisAmount +
                     "\t" + bonus + "\n";
             totalAmount += thisAmount;
             totalBonus += bonus;
         }
-        //добавить нижний колонтитул
         result += "Сумма счета составляет " + totalAmount + "\n";
         result += "Вы заработали " + totalBonus + " бонусных балов";
-        //Запомнить бонус клиента
         _customer.receiveBonus(totalBonus);
+        return result;
+    }
+
+    boolean isDiscounting(Item each){
+        return (each.getGoods().getPriceCode() == Goods.SPECIAL_OFFER) && each.getQuantity() > 1;
+    }
+
+    String tableHeaderOutput(){
+        String result = "Счет для " + _customer.getName() + "\n";
+        result += "\t" + "Название" + "\t" + "Цена" +
+                "\t" + "Кол-во" + "\t" + "Стоим" + "\t" + "Скидк" +
+                "\t" + "Сумма" + "\t" + "Бонус" + "\n";
         return result;
     }
 }
